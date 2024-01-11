4a45f331a95e14161c7c7a7e18158f5a02ac4150
fqueta <ger.maisaqui1@gmail.com>
2024-01-11 12:54:32 -0300

    11/01/2024

--- resources/views/site/leiloes/list.blade.php
+++ resources/views/site/leiloes/list.blade.php
@@ -99,12 +99,32 @@ $urlB = App\Qlib\Qlib::get_slug_post_by_id(37); //link da pagina para cosulta de
                     <h2 class="title-page text-center mb-5">Leilões disponíveis</h2>
                 </div>
             </div>
-            @include('site.leiloes.list_grid',['dados'=>@$dados,'config'=>$config])
-                {{-- @php
-                //listar leilões para o plublico
-                    $ret = view('site.leiloes.list_grid',['dados'=>@$dados,'config'=>$config]);
-                    echo $ret;
-                @endphp --}}
+            {{-- @include('site.leiloes.list_grid',['dados'=>@$dados,'config'=>$config]) --}}
+            <div class="items-list mb-4">
+                @php
+                $li = false;
+                $class_lc = new App\Http\Controllers\LeilaoController;
+                @endphp
+                @if (is_object($dados) || is_array($dados))
+                    @foreach ($dados as $k=>$v )
+                        @php
+                            // $v['src'] = App\Qlib\Qlib::get_thumbnail_link($v['ID']);
+                            // $v['link'] = App\Qlib\Qlib::get_the_permalink($v['ID'],$v);
+                            // $v['link_edit_admin'] = (new App\Http\Controllers\LeilaoController)->get_link_edit_admin($v['ID'],$v);
+                            $info_termino = $class_lc->info_termino($v['ID']);
+                            $li .= view('site.leiloes.grid',[
+                                'v'=>$v,
+                                'info_termino'=>$info_termino['html'],
+                                'arr_termino'=>$info_termino
+                            ]);
+                        @endphp
+                    @endforeach
+                    @php
+                        echo $li;
+                    @endphp
+                @endif
+            </div>
+
 
             {{-- <div class="items-nav d-flex justify-content-center">
                 <nav aria-label="Navegação de leilões">
