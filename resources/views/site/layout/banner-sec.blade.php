458e0efe8ec5b39d0466fe402a81715a7a09037f
fqueta <ger.maisaqui1@gmail.com>
2024-01-12 10:00:44 -0300

    12/01/2024

--- resources/views/site/layout/banner-sec.blade.php
+++ resources/views/site/layout/banner-sec.blade.php
@@ -9,8 +9,8 @@
 <section class="banner-page">
     <div class="container py-5">
         <div class="banner-page-content">
-            <h2>{{$post['post_title']}}</h2>
-            <p>{{$post['post_execerpt']}}</p>
+            <h2>{{@$post['post_title']}}</h2>
+            <p>{{@$post['post_execerpt']}}</p>
         </div>
     </div>
     <div class="banner-page-overlay"></div>
