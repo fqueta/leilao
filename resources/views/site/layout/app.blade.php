1c353da66f1e9864c1342b679f8c7bbb7be248f9
fqueta <ger.maisaqui1@gmail.com>
2024-05-08 12:33:50 -0300

    08/05/2024

--- resources/views/site/layout/app.blade.php
+++ resources/views/site/layout/app.blade.php
@@ -11,8 +11,8 @@
   <meta content="" name="keywords">
 
   <!-- Favicons -->
-  <link href="{{url('/')}}/assets/img/favicon.png" rel="icon">
-  <link href="{{url('/')}}/assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+  <link href="{{url('/')}}/vendor/adminlte/dist/img/AdminLTELogo.png" rel="icon">
+  <link href="{{url('/')}}/vendor/adminlte/dist/img/AdminLTELogo.png" rel="apple-touch-icon">
 
   <!-- Google Fonts -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,700,700i&display=swap" rel="stylesheet">
